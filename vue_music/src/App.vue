d1e0726a0f3a2af30cea4ebeff93aa66e1114349
spring <1037518778@qq.com>
2019-06-19 17:02:28 +0800

    配置文件及样式配置

--- vue_music/src/App.vue
+++ vue_music/src/App.vue
@@ -1,23 +1,18 @@
 <template>
   <div id="app">
-    hello world
+    <m-header></m-header>
     <router-view/>
   </div>
 </template>
 
 <script>
+import mHeader from '@/components/m-header/m-header.vue'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    mHeader
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped lang="stylus" rel="stylesheet/stylus">
 </style>
--- vue_music/src/main.js
+++ vue_music/src/main.js
@@ -1,9 +1,16 @@
+// es6编译功能 babel-polyfill
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store/store.js'
+import fastclick from 'fastclick'
+
 // 引入index.styl样式
 import 'common/stylus/index.styl'
+
+// 使整个body页面下没有300ms的延迟
+fastclick.attach(document.body)
 
 Vue.config.productionTip = false
 
